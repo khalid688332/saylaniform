ae77b822c387d87fca0b891ec825cd13921e679e
khalid688332 <ah6883322@gmail.com>
2024-11-17 00:20:10 +0500

    all file uploaded

---
+++ saylaniform/script.js
@@ -0,0 +1,37 @@
+function submit(){
+        var country=document.getElementById("country").value
+        console.log(country)
+        var city= document.getElementById("city").value
+        console.log(city);
+        var course= document.getElementById("course").value
+        console.log(course)
+        var computerProficiency= document.getElementById("computerProficiency").value
+        console.log(computerProficiency)
+        var fullName = document.getElementById("fullName").value
+        console.log(fullName)
+        var fatherName=document.getElementById("fatherName").value
+        console.log(fatherName)
+        var email= document.getElementById("email").value
+        console.log(email)
+        var phoneNumber=document.getElementById("phoneNumber").value
+        console.log(phoneNumber)
+        var cnic=document.getElementById("cnic").value
+        console.log(cnic)
+        var fatherCnic=document.getElementById("fatherCnic").value
+        console.log(fatherCnic)
+        var dob=document.getElementById("dob").value
+        console.log(dob)
+        var gender=document.getElementById("gender").value
+        console.log(gender)
+        var address=-document.getElementById("address").value
+        console.log(address)
+        var lastQualification=document.getElementById("lastQualification").value
+        console.log(lastQualification)
+        var laptop=document.getElementById("laptop").value
+        console.log(laptop)
+}
+
+
+    
+    
+
